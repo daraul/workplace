d2c766d7f9ad1e5b1306b3bf52a510ba8479b743
thurst306 <thurst306@gmail.com>
2014-04-11 22:51:49 +0000

    aded logo and menu. updated header

---
+++ require/menu.php
@@ -0,0 +1,11 @@
+<ul id="main_menu">
+    <li><a href="workplace.php" title="Home page" alt="Head to the homepage. You'll have to log in again, though">Home</a></li>
+    <li>
+        <a href="#" title="Your work to be done" alt="Your work to be done">Work</a>
+        <ul>
+            <li><a href="#" title="All jobs to be done" alt="Jobs to be done">Pending jobs</a></li>
+            <li><a href="addnewjob.php" title="Add a new job" alt="Add a new job">New job</a></li>
+        </ul>
+    </li>
+    <li><a href="clients.php" title="Clients" alt="Click here to see your clients!">Clients</a></li>
+</ul>
