8a7c2db6e13ea5e6ae5aacdd9a91fb664b2083a9
thurst306 <thurst306@gmail.com>
2014-04-11 22:42:57 +0000

    updated index for footer and dependencies

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
         <?php require "tables/todotable.php"; ?>
     </div>
     <footer>
-        <?php require "includes/footer.php" ?>
+        <?php require "require/footer.php" ?>
         <h3>Footnotes!</h3>
         <p>Alright, footnotes. Well so far this works. You can log in, but it won't remember you if you close the page. I'll have to figure out how to do that now...
         <br/>Click <a href="http://thurst306.kd.io/">here</a> if you get bored and want to go back home.</p>
     </footer>
 </body>
-    <?php require "js/basic_dependencies.php" ?>
+    <?php require "require/basic_dependencies.php" ?>
 </html>
