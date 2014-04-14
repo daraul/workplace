3a593ae85ceeae6a003cb7c33749dabf52b1fd59
thurst306 <thurst306@gmail.com>
2014-04-14 10:42:10 -0700

    Updated the head require

--- clients.php
+++ clients.php
@@ -1,17 +1,17 @@
 <?php 
     $page_title = "Clients";
-    require "includes/head.php"; 
+    require "require/head.php"; 
 ?>
 </head>
 <body>
     <?php require "includes/header.php" ?>
     <div class="body_wrapper">
         <h2 class="page_title">Clients</h2>
-        <?php require "includes/clientstable.php" ?>
+        <?php require "tables/clientstable.php" ?>
     </div>
 
     <footer>
-        <?php require ("includes/footer.php"); ?>
+        <?php require ("require/footer.php"); ?>
         <h3>Client footnotes!</h3>
         <p>Well, this is where the clients should go. I haven't added the table as yet, though.</p>
     </footer>
