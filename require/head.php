f5cfb9b0297c5da4526df0aa9fe2999fbfbf5d0f
thurst306 <thurst306@gmail.com>
2014-04-14 11:29:29 -0700

    Added fonts back

--- require/head.php
+++ require/head.php
@@ -4,3 +4,4 @@
         <meta charset="utf-8">
         <title>Workplace - <?php echo $page_title ?></title>
         <link rel="stylesheet" type="text/css" href="css/main_style.css">
+        <link href='http://fonts.googleapis.com/css?family=Bevan|Roboto:400,400italic' rel='stylesheet' type='text/css'>
