ee0cce6b47a5d93d61da3d4e798528af640a9305
thurst306 <thurst306@gmail.com>
2014-05-07 22:36:30 +0000

    Update information displays on submit

--- forms/update_job_info.php
+++ forms/update_job_info.php
@@ -6,29 +6,33 @@
     $clients = mysqli_query($update_con, "SELECT * FROM Clients");
     //Jobs
     $update_select_job_names = mysqli_query($update_con, "SELECT * FROM worktodo WHERE JobID = " . $job_id);
-    $job_name = [];
-    $job_desc = [];
-    $job_lead = [];
-    $job_client = [];
-    $job_start = [];
-    $job_due = [];
-    $job_status = [];
-    while ($row = mysqli_fetch_array($update_select_job_names)){
-        array_push($job_name, $row['JobTitle']);
-        array_push($job_desc, $row['Description']);
-        array_push($job_start, $row['StartDate']);
-        array_push($job_due, $row['DueDate']);
-        array_push($job_lead, $row['employeeID']);
-        array_push($job_client, $row['ClientID']);
-        array_push($job_status, $row['currentStatus']);
+    //Check for submission
+    if(isset($_POST["submit"])){
+        $job_name = $_POST['jobname'];
+        $job_desc = $_POST['jobdesc'];
+        $job_start = $_POST['startdate'];
+        $job_due = $_POST['duedate'];
+        $job_lead = $_POST['startdate'];
+        $job_client = $_POST['client'];
+        $job_status = $_POST['status'];
+    } else {
+        while ($row = mysqli_fetch_array($update_select_job_names)){
+            $job_name = $row['JobTitle'];
+            $job_desc = $row['Description'];
+            $job_start = $row['StartDate'];
+            $job_due = $row['DueDate'];
+            $job_lead = $row['employeeID'];
+            $job_client = $row['ClientID'];
+            $job_status = $row['status'];
+        }
     }
 ?>
 <form class="add_info" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); echo "?key=" . $job_id;?>" method="post">
     <label>Job Name: 
-        <input type="text" id="jobname" name="jobname" class="text_input" value="<?php echo $job_name[0]; ?>" required />
+        <input type="text" id="jobname" name="jobname" class="text_input" value="<?php echo $job_name; ?>" required />
     </label>
     <label>Job Description: 
-        <textarea rows="4" id="jobdesc" columns="50" name="jobdesc" required /><?php echo $job_desc[0]; ?></textarea>
+        <textarea rows="4" id="jobdesc" columns="50" name="jobdesc" required /><?php echo $job_desc; ?></textarea>
     </label>
     <label>Lead: 
         <select id="employee" name="employee" required>
@@ -54,22 +58,21 @@
         <input type="text" id="startdate" value="<?php echo $job_start[0]; ?>" name="startdate" class="datepicker" required />
     </label>
     <label>Due Date: 
-        <input type="text" id="duedate" class="datepicker" value="<?php echo $job_due[0]; ?>" name="duedate"required />
+        <input type="text" id="duedate" class="datepicker" value="<?php echo $job_due[0]; ?>" name="duedate" required />
     </label>
     <label>Status: 
-        <select id="status" value="Complete" name="status" required>
+        <select id="status" name="status" required>
             <?php require "includes/statuses.php" ?>
         </select>
     </label>
-    <input type="submit" id="submit" name="submit" value="Add job" />
+    <input type="submit" id="submit" name="submit" value="Update job" />
 </form>
 <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
 <link rel="stylesheet" href="css/forms.css" />
-<!--<script src="//code.jquery.com/jquery-1.9.1.js"></script>-->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
 <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 <script>
-    $("select#employee option[value='<?php echo $job_lead[0] ?>']").attr("selected", "");
-    $("select#client option[value='<?php echo $job_client[0] ?>']").attr("selected", "");
-    $("select#status option[value='<?php echo $job_status[0] ?>']").attr("selected", "");
+    $("select#employee option[value='<?php echo $job_lead ?>']").attr("selected", "");
+    $("select#client option[value='<?php echo $job_client ?>']").attr("selected", "");
+    $("select#status option[value='<?php echo $job_status ?>']").attr("selected", "");
 </script>
