8ee639b917f914e4caab5d8a2253b043bd5adf39
thurst306 <thurst306@gmail.com>
2014-05-10 13:45:00 +0000

    Date pickers now use image

--- forms/update_job_info.php
+++ forms/update_job_info.php
@@ -10,16 +10,30 @@
     $clients = mysqli_query($update_con, "SELECT * FROM Clients");
     //Jobs
     $update_select_job_names = mysqli_query($update_con, "SELECT * FROM worktodo WHERE JobID = " . $job_id);
+    //Test input function
+    function test_input($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    };
     //Check for submission
     if(isset($_POST["submit"])){
+        $job_name = test_input($_POST['jobname']);
+        $job_desc = test_input($_POST['jobdesc']);
+        $job_start = test_input($_POST['startdate']);
+        $job_due = test_input($_POST['duedate']);
+        $job_lead = test_input($_POST['startdate']);
+        $job_client = test_input($_POST['client']);
+        $job_status = test_input($_POST['status']);
         //Check that each and every submission isn't somehow empty
         if(empty($_POST['jobname']) || empty($_POST['jobdesc']) || empty($_POST['startdate']) || empty($_POST['duedate']) || empty($_POST['startdate']) || empty($_POST['client']) || empty($_POST['status'])){
-        echo "
-            <script>
-                //console.log(\"Something is empty!\");
-                alert(\"ALL fields are required! The form has been reset.\");
-            </script>
-        ";
+            echo "
+                <script>
+                    //console.log(\"Something is empty!\");
+                    alert(\"ALL fields are required! The form has been reset.\");
+                </script>
+            ";
             while ($row = mysqli_fetch_array($update_select_job_names)){
                 $job_name = $row['JobTitle'];
                 $job_desc = $row['Description'];
@@ -29,17 +43,20 @@
                 $job_client = $row['ClientID'];
                 $job_status = $row['currentStatus'];
             }
-        } else {
+        } else if(preg_match("[A-Za-z0-9\s]+",$jobname)) {
             //Submission successful, update variables to submitted data
-            $job_name = $_POST['jobname'];
-            $job_desc = $_POST['jobdesc'];
-            $job_start = $_POST['startdate'];
-            $job_due = $_POST['duedate'];
-            $job_lead = $_POST['startdate'];
-            $job_client = $_POST['client'];
-            $job_status = $_POST['status'];
+            echo "
+                <script>
+                    console.log('Submission successful!');
+                </script>
+            ";
         }
     } else {
+        echo "
+            <script>
+                console.log('No submission. Default form info loaded.');
+            </script>
+        ";
         while ($row = mysqli_fetch_array($update_select_job_names)){
             $job_name = $row['JobTitle'];
             $job_desc = $row['Description'];
@@ -53,15 +70,15 @@
 ?>
 <form class="add_info" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); echo "?key=" . $job_id;?>" method="post">
     <label>Job Name: 
-        <input type="text" id="jobname" name="jobname" class="text_input" value="<?php echo $job_name; ?>" />
+        <input type="text" id="jobname" name="jobname" class="text_input" value="<?php echo $job_name; ?>" pattern="[A-Za-z0-9\s]+" required />
         <p id="error-text" class="error-text">This is required!</p>
     </label>
     <label>Job Description: 
-        <textarea rows="4" id="jobdesc" columns="50" name="jobdesc" /><?php echo $job_desc; ?></textarea>
+        <textarea rows="4" id="jobdesc" columns="50" name="jobdesc" required /><?php echo $job_desc; ?></textarea>
         <p class="error-text">This is required!</p>
     </label>
     <label>Lead: 
-        <select id="employee" name="employee">
+        <select id="employee" name="employee" required>
             <option disabled style="display: none;"></option>
             <?php
                 while($row = mysqli_fetch_array($employees)){
@@ -72,7 +89,7 @@
         <p class="error-text">This is required!</p>
     </label>
     <label>Client: 
-        <select id="client" name="client">
+        <select id="client" name="client" required>
             <option selected disabled style="display: none;"></option>
             <?php
                 while($client_row = mysqli_fetch_array($clients)){
@@ -83,15 +100,15 @@
         <p class="error-text">This is required!</p>
     </label>
     <label>Start Date: 
-        <input type="text" id="startdate" value="<?php echo $job_start[0]; ?>" name="startdate" class="datepicker" />
+        <input type="text" id="startdate" value="<?php echo $job_start; ?>" name="startdate" class="datepicker" required />
         <p class="error-text">This is required!</p>
     </label>
     <label>Due Date: 
-        <input type="text" id="duedate" class="datepicker" value="<?php echo $job_due[0]; ?>" name="duedate" />
+        <input type="text" id="duedate" class="datepicker" value="<?php echo $job_due; ?>" name="duedate" required />
         <p class="error-text">This is required!</p>
     </label>
     <label>Status: 
-        <select id="status" name="status">
+        <select id="status" name="status" required>
             <option selected disabled style="display: none;"></option>
             <?php require "includes/statuses.php" ?>
         </select>
@@ -99,6 +116,17 @@
     <input type="submit" id="submit" name="submit" value="Update job" />
 </form>
 <script>
+    /*$(function() {
+        $(".datepicker").datepicker();
+        $( ".datepicker" ).datepicker("option", "dateFormat", "yy-mm-dd");
+    });*/
+    $(function() {
+        $( ".datepicker" ).datepicker({
+          showOn: "button",
+          buttonImage: "images/calendar.gif",
+          buttonImageOnly: true
+        });
+    });
     $("select#employee option[value='<?php echo $job_lead ?>']").attr("selected", "");
     $("select#client option[value='<?php echo $job_client ?>']").attr("selected", "");
     $("select#status option[value='<?php echo $job_status ?>']").attr("selected", "");
