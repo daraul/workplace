49c29ef9bc6a42463b7a9314d9b4371232a151e9
thurst306 <thurst306@gmail.com>
2014-05-13 00:17:04 +0000

    Update query added

--- forms/update_job_info.php
+++ forms/update_job_info.php
@@ -19,6 +19,7 @@
     };
     //Check for submission
     if(isset($_POST["submit"])){
+        //Test all input
         $job_name = test_input($_POST['jobname']);
         $job_desc = test_input($_POST['jobdesc']);
         $job_start = test_input($_POST['startdate']);
@@ -28,6 +29,7 @@
         $job_status = test_input($_POST['status']);
         //Check that each and every submission isn't somehow empty
         if(empty($_POST['jobname']) || empty($_POST['jobdesc']) || empty($_POST['startdate']) || empty($_POST['duedate']) || empty($_POST['startdate']) || empty($_POST['client']) || empty($_POST['status'])){
+            //Alert and kill the process if something is empty
             echo "
                 <script>
                     //console.log(\"Something is empty!\");
@@ -43,8 +45,9 @@
                 $job_client = $row['ClientID'];
                 $job_status = $row['currentStatus'];
             }
+            //Otherwise check the formats for each
         } else if (!preg_match("^.*[A-Za-z0-9\s]+.*$",$job_name) || !preg_match("^.*[2]{1}[01]{1}[0-9]{2}\-(0[1-9]|1[012])\-(0[1-9]|1[0-9]|2[0-9]|3[01]).*$", $job_start) || !preg_match("^.*[2]{1}[01]{1}[0-9]{2}\-(0[1-9]|1[012])\-(0[1-9]|1[0-9]|2[0-9]|3[01])*.$", $job_due)) {
-            //Submission syntax is wrong!
+            //Submission syntax is wrong! Kill it with fire!
             echo "
                 <script>
                     console.log(\"Submission syntax is wrong!\");
@@ -67,6 +70,7 @@
                     console.log('Submission successful!');
                 </script>
             ";
+            $insert = mysqli_query($update_con, "UPDATE worktodo SET 'currentStatus' = '" . $job_status . "', 'ClientID' = '" . $job_client . "', employeeID' = '" . $job_lead . "', 'StartDate' = '" . $job_start . "' 'DueDate' = '" . $job_due . "', 'JobTitle' = '" . $job_name . "', 'Description' = '" . $job_desc . "' WHERE 'JobID' = '" . $job_id . "'");
         }
     } else {
         echo "
