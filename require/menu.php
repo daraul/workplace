70e22b934c8204c0fa72e02bf5310b3fbdbe77c0
thurst306 <thurst306@gmail.com>
2014-05-16 16:45:30 +0000

    Removed 2 menu items not being used

--- require/menu.php
+++ require/menu.php
@@ -1,11 +1,11 @@
 <ul id="main_menu">
     <li><a href="index.php" title="Home page" alt="Head to the homepage. You'll have to log in again, though">Home</a></li>
     <li>
-        <a href="#" title="Your work to be done" alt="Your work to be done">Work</a>
+        <!--<a href="#" title="Your work to be done" alt="Your work to be done">Work</a>
         <ul>
             <li><a href="#" title="All jobs to be done" alt="Jobs to be done">Pending jobs</a></li>
-            <li><a href="add_new_job.php" title="Add a new job" alt="Add a new job">New job</a></li>
-        </ul>
+            <li>--><a href="add_new_job.php" title="Add a new job" alt="Add a new job">New job</a></li>
+        <!--</ul>-->
     </li>
     <li><a href="clients.php" title="Clients" alt="Click here to see your clients!">Clients</a></li>
 </ul>
