b30154e4b7b64df17d1e45d988207a6ddfbf8ae3
thurst306 <thurst306@gmail.com>
2014-05-17 07:56:16 -0700

    Update new job form link

--- new_job.php
+++ new_job.php
@@ -9,7 +9,7 @@
     <div class="body_wrapper">
         <h2 class="page_title">New job information</h2>
         <p>This&#32;is&#32;where&#32;you&#39;ll&#32;add&#32;a&#32;new&#32;job&#46;</p>
-        <?php require "forms/new_job.php" ?>
+        <?php require "forms/add_new_job.php" ?>
     </div>
     <footer>
         <?php require "require/footer.php"; ?>
