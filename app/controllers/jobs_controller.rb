685ae956ea5e067a9817728904fe4de51a1b7428
Daraul Harris <thurst306@gmail.com>
2014-08-05 14:15:02 +0000

    Jobs can now be created

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,16 @@ class JobsController < ApplicationController
     def index
         @jobs = Job.all
     end
+    
+    def new
+        @job = Job.new
+    end
+    
+    def create
+        @job = Job.new
+        @job.title = params[:job][:title]
+        @job.save
+        
+        redirect_to jobs_path(@jobs)
+    end
 end
