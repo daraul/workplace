0b88ce9555521b7e99f8e02f459d27808cd0a022
Daraul Harris <thurst306@gmail.com>
2014-08-06 17:53:52 +0000

    Employee CRUD actions complete

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,42 @@
 class EmployeesController < ApplicationController
+    include EmployeesHelper
+    
     def index
         @employees = Employee.all
+    end
+    
+    def new
+        @employee = Employee.new
+    end
+    
+    def create
+        @employee = Employee.new(employee_params)
+        @employee.save
+        
+        redirect_to employees_path
+        
+        flash.notice = "Employee '#{@employee.first_name} #{@employee.last_name}' created!"
+    end
+    
+    def show
+        @employee = Employee.find(params[:id])
+    end
+    
+    def destroy
+        @employee = Employee.find(params[:id])
+        @employee.destroy
+        
+        flash.notice = "Employee '#{@employee.first_name} #{@employee.last_name}' deleted!"
+        
+        redirect_to employees_path
+    end
+    
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        
+        flash.notice = "Employee '#{@employee.first_name} #{@employee.last_name}' updated!"
+        
+        redirect_to employees_path
     end
 end
--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,2 +1,7 @@
 module EmployeesHelper
+    
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name)
+    end
+    
 end
