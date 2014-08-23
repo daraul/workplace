9ee01ec3b3b5093ad4bde5e77811336b23ad6e91
einkelflugle <cluckyferret@gmail.com>
2014-08-23 21:25:02 +1000

    Fixed duplication error in employee assignments
    Users can no longer be assigned to the same project twice.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -7,10 +7,14 @@ class AssignmentsController < ApplicationController
     
     def create
         @assignment = Assignment.new(assignment_params)
-        @assignment.save
         
-        redirect_to projects_path
-        
-        flash.notice = "Job assigned!"
+        # Verify that the assignment saved correctly and without errors.
+        # If not, render the page again and display the errors.
+        if @assignment.save
+            flash.notice = "Job assigned!"
+            redirect_to projects_path
+        else
+            render 'new'
+        end
     end
 end
