56a12df481697741d28d6e1aea255bf7743cdc22
Daraul Harris <thurst306@gmail.com>
2014-11-04 18:43:50 +0000

    Deleted assignment model

--- app/models/assignment.rb
+++
@@ -1,8 +0,0 @@
-class Assignment < ActiveRecord::Base
-    belongs_to :project
-    belongs_to :user
-
-    # Make sure the assignment is unique.
-    # ie, the same user cannot be assigned to one project twice.
-    validates_uniqueness_of :user, scope: :project, message: "is already assigned to this project!"
-end
