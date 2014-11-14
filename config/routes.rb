62c20c307a5f2d5550f9fda9ce8170283bf10129
Daraul Harris <thurst306@gmail.com>
2014-11-14 19:08:33 +0000

    Updated route to use users/registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+    devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }, path_names: {sign_in: "login", sign_out: "logout"}
     
     devise_scope :user do
         authenticated :user do
