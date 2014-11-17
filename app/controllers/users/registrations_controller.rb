9fb8368ba5bbc830da2181d670af3bc1a1a0ef49
Daraul Harris <thurst306@gmail.com>
2014-11-17 16:34:42 +0000

    Temporary auto-role add on user#create

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,9 +8,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+    def create
+        super
+        
+        @user.add_role "view_jobs"
+    end
 
   # GET /resource/edit
   # def edit
