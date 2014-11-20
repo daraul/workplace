41f9d26f82dc1470af221b99417886aa3e7b4670
Daraul Harris <thurst306@gmail.com>
2014-11-20 13:48:14 +0000

    Organizations_users table created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ActiveRecord::Base
-  rolify
+    rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable
          
-         def full_name
-             "#{first_name} #{last_name}"
-         end
+    def full_name
+        "#{first_name} #{last_name}"
+    end
          
-        has_many :jobs
-        has_many :time_entries
-        has_many :projects, through: :jobs
-        has_and_belongs_to_many :organizations
+    has_many :jobs
+    has_many :time_entries
+    has_many :projects, through: :jobs
+    has_and_belongs_to_many :organizations
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141120132240) do
+ActiveRecord::Schema.define(version: 20141120133652) do
 
   create_table "jobs", force: true do |t|
     t.string   "title"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20141120132240) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "organizations_users", id: false, force: true do |t|
+    t.integer "organization_id"
+    t.integer "user_id"
   end
 
   create_table "projects", force: true do |t|
