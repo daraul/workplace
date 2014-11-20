b08c21a11d3a795b6ad87c9ce84f38642fec5c47
Daraul Harris <thurst306@gmail.com>
2014-11-20 16:47:28 +0000

    Projecst are now tied to organizations

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20141120133652) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
+    t.integer  "organization_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
