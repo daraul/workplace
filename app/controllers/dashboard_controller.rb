df9bdfd510d209b0227101be7d860faab774c8ea
Daraul Harris <thurst306@gmail.com>
2014-12-12 14:02:08 +0000

    Uncompleted jobs show up on the dashboard I had to also update the jobs policy to allow users to edit jobs that they are assigned to. In the future I guess administrators should be able to edit all their organizations jobs, too, buit that 's a little way down the road

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,20 @@
 class DashboardController < ApplicationController
+    include DashboardHelper
+    
+    before_filter :is_admin
+    
+    def is_admin
+        if user_signed_in?
+            true
+        else
+            flash.notice = "You need to log in to do that!"
+        
+            redirect_to unauthenticated_root_path
+        end
+    end
+    
+    def index
+        #Only show the user uncompleted jobs 
+        @jobs = current_user.jobs.select { |key, value| key.complete == false }
+    end 
 end
