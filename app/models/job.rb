69d841564cf2010cd2244533649bab3074c3dd52
Daraul Harris <thurst306@gmail.com>
2014-12-20 15:11:14 +0000

    THe jobs model wasn't validating the project or the job id I fixed that

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,8 +3,8 @@ class Job < ActiveRecord::Base
     
     validates :title, presence: true, length: { in: 5..60, message: "must be kept between 5 to 60 characters" }, format: { with: /[A-z0-9\.\\\/\(\)\?\$\&\s]{5,60}/, message: "can only use upper & lower case letters, numbers and these special characters: . \ / ( ) ? $ &" }
     validates :description, presence: true, length: { in: 5..500, message: "must be kept between 5 to 500 characters" }, format: { with: /[A-z0-9\.\\\/\(\)\?\$\&\,\s]{5,500}/, message: "can only use upper & lower case letters, numbers and these special characters: . \ / ( ) ? $ & ," }
-    validates :user_id, presence: true
-    validates :project_id, presence: true
+    validates :user_id, presence: true, format: { with: /\d+/, message: "can only be a number."  }
+    validates :project_id, presence: true, format: { with: /\d+/, message: "can only be a number."  }
     
     has_many :time_entries, dependent: :destroy
     belongs_to :user
