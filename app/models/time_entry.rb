10e205414ff10c706498594f83791330d4056c60
Daraul Harris <thurst306@gmail.com>
2014-12-21 18:46:17 +0000

    Forgot to commit the model changes

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,4 +1,10 @@
 class TimeEntry < ActiveRecord::Base
+    validates :description, presence: true, length: { in: 5..500, message: "must be kept between 5 to 500 characters" }, format: { with: /[A-z0-9\.\\\/\(\)\?\$\&\,\s]{5,500}/, message: "can only use upper & lower case letters, numbers and these special characters: . \ / ( ) ? $ & ," }
+    validates :start, presence: true, format: { with: /\d+/, message: "can only be a number."  }
+    validates :finish, presence: true, format: { with: /\d+/, message: "can only be a number."  }
+    validates :job_id, presence: true, format: { with: /\d+/, message: "can only be a number."  }
+    validates :user_id, presence: true, format: { with: /\d+/, message: "can only be a number."  }
+    
     belongs_to :job
     belongs_to :user
 end
