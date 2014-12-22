a22b9861b609196b423f588594c93954577bccd1
Daraul Harris <thurst306@gmail.com>
2014-12-22 15:13:24 +0000

    Updated the description regexes for both of these The regex previously didn't work to validate these properly

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,11 +1,11 @@
 class TimeEntry < ActiveRecord::Base
-    validates :description, presence: true, length: { in: 5..500, message: "must be kept between 5 to 500 characters" }, format: { with: /[A-z0-9\.\\\/\(\)\?\$\&\,\s]{5,500}/, message: "can only use upper & lower case letters, numbers and these special characters: . \ / ( ) ? $ & ," }
+    validates :description, presence: true, length: { in: 5..500, message: "must be kept between 5 to 500 characters" }, format: { with: /^[A-z0-9\.\\\/\(\)\?\$\&\,\s]{5,500}$/, :multiline => true, message: "can only use upper & lower case letters, numbers and these special characters: . \ / ( ) ? $ & ," }
     validates :start, presence: true, format: { with: /\d+/, message: "can only be a number."  }
     validates :finish, presence: true, format: { with: /\d+/, message: "can only be a number."  }
     validates :job_id, presence: true, format: { with: /\d+/, message: "can only be a number."  }
     validates :user_id, presence: true, format: { with: /\d+/, message: "can only be a number."  }
     
-    validate :start_is_before_finish, on: :create, message: "testing!"
+    validate :start_is_before_finish, on: :create
     
     def start_is_before_finish
         if (start && finish) && finish < start
