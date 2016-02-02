c820fc5a1d56c6fab715ab5cfdd5c53e9787dbdd
Daraul <thurst306@gmail.com>
2016-02-02 17:59:20 +0000

    Check completed before validating #21 I didn't realize this prevented updating the record at all unless all children were completed

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,10 @@
 class Todo < ActiveRecord::Base
+    after_save :complete_parents_if_siblings_completed
+    
     has_and_belongs_to_many :children, class_name: "Todo", join_table: "todos_todos", foreign_key: "parent_id", association_foreign_key: :child_id
     has_and_belongs_to_many :parents, class_name: "Todo", join_table: "todos_todos", foreign_key: "child_id", association_foreign_key: :parent_id
+    
+    has_many :siblings, :through => :parents
     
     validates :title, presence: true, length: { in: 5..35, message: "must be 5 to 35 characters long!" }, format: { with: /\A[A-Z][\w\!\,\. ]+\z/, message: "must be alphanumeric, begin with a capital letter and can include white space or these characters: !,." } 
     
@@ -12,6 +16,12 @@ class Todo < ActiveRecord::Base
     validate :disallow_identical_parent_child_reference
     
     validate :disallow_completion_unless_all_children_completed
+    
+    def complete_parents_if_siblings_completed
+        #parents.each do |parent|
+            true
+        #end 
+    end 
     
     def disallow_self_referential_child
         if child_ids.include? id 
@@ -35,10 +45,12 @@ class Todo < ActiveRecord::Base
     end 
     
     def disallow_completion_unless_all_children_completed
-        children.each do |child|
-            if child.completed != true 
-                errors.add(:completed, 'cannot be true unless all children are completed!')
-                return 
+        if completed == true 
+            children.each do |child|
+                if child.completed != true 
+                    errors.add(:completed, 'cannot be true unless all children are completed!')
+                    return 
+                end 
             end 
         end 
     end 
