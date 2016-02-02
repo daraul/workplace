14b9c18b0751cc9c1619c4c34d14e1f4c2377a86
Daraul <thurst306@gmail.com>
2016-02-02 14:44:12 +0000

    Disallow parent/child self referencing #19 Hope I referenced the issue correctly

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -3,4 +3,19 @@ class Checklist < ActiveRecord::Base
     has_and_belongs_to_many :parents, class_name: "Checklist", join_table: "checklists_checklists", foreign_key: "child_id", association_foreign_key: :parent_id
     
     validates :title, presence: true
+    
+    validate :disallow_self_referential_child
+    validate :disallow_self_referential_parent
+    
+    def disallow_self_referential_child
+        if child_ids.include? id 
+            errors.add(:child_id, 'cannot self reference!')
+        end 
+    end 
+    
+    def disallow_self_referential_parent
+        if parent_ids.include? id 
+            errors.add(:parent_id, "cannot self reference!")
+        end 
+    end 
 end
