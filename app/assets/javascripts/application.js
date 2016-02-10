27d89d75857182d52d05fb87265e6039bac8a3ca
Daraul <thurst306@gmail.com>
2016-02-10 15:36:44 +0000

    Add rstacruz/nprogress #28 Now there's an indication when the page is using ajax to load content Thanks to @rstacruz for this.https://github.com/rstacruz/nprogress/

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require nprogress
 //= require_tree .
+
+$(document).on('page:fetch',   function() { NProgress.start(); });
+$(document).on('page:change',  function() { NProgress.done(); });
+$(document).on('page:restore', function() { NProgress.remove(); });
